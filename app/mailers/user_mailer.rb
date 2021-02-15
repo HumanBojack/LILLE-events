e0c1c23fd8cd9347d82a8057f018876a1f87ae27
HumanBojack <spychalar59@gmail.com>
2021-02-15 14:33:22 +0100

    Mailer for new attendance

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,9 @@ class UserMailer < ApplicationMailer
 		@user = user
 		mail(to: @user.email, subject: "Bienvenue chez nous !")
 	end
+
+	def attendance_email(attendance)
+		@attendance = attendance
+		mail(to: @attendance.event.user.email, subject: "Votre évènement: #{@attendance.event.title}")
+	end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,9 @@
 class Attendance < ApplicationRecord
+	after_create :attendance_email
 	belongs_to :user
 	belongs_to :event
+
+	def attendance_email
+		UserMailer.attendance_email(self).deliver_now
+	end
 end
